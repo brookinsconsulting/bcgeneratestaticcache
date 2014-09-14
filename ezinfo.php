84a0dad4db465e64ea89863df0eddfd3b2fedcb4
Brookins Consulting <info@brookinsconsulting.com>
2014-09-14 17:31:52 -0500

    Added: Added ezinfo.php file

---
+++ ezinfo.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File containing the bcgeneratestaticcache copyright information file
+ *
+ * @copyright Copyright (C) 1999 - 2014 Brookins Consutling and 2007 Damien POBEL
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ * @package bcgeneratestaticcache
+ */
+
+class bcgeneratestaticcacheInfo
+{
+    function info()
+    {
+        return array( 'Name' => "<a href='http://projects.ez.no/bcgeneratestaticcache'>BC Generate Static Cache</a>",
+                      'Version' => "0.5.0",
+                      'Copyright' => "Copyright (C) 1999 - 2014 <a href='http://brookinsconsulting.com' title='Brookins Consulting'>Brookins Consulting</a>, 2007 <a href='https://github.com/dpobel' title='Damien Pobel'>Damien Pobel</a>",
+                      'License' => "GNU General Public License v2.0",
+                      'info_url' => "https://github.com/brookinsconsulting/bcgeneratestaticcache"
+                    );
+    }
+}
+
+?>
