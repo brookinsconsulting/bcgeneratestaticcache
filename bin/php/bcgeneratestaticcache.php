2f9df29952b9f00adaa14cd9b9fdd4d406664066
Brookins Consulting <info@brookinsconsulting.com>
2014-09-14 17:36:12 -0500

    Updated: Updated extension copyright headers

--- bin/php/bcgeneratestaticcache.php
+++ bin/php/bcgeneratestaticcache.php
@@ -1,22 +1,13 @@
 #!/usr/bin/env php
 <?php
-// SOFTWARE NAME: bcgeneratestaticcache
-// COPYRIGHT NOTICE: Copyright (C) 2007 Damien POBEL and 1999 - 2014 Brookins Consulting
-// SOFTWARE LICENSE: GNU General Public License v2.0
-// NOTICE: >
-//   This program is free software; you can redistribute it and/or
-//   modify it under the terms of version 2.0  of the GNU General
-//   Public License as published by the Free Software Foundation.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of version 2.0 of the GNU General
-//   Public License along with this program; if not, write to the Free
-//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//   MA 02110-1301, USA.
+/**
+ * File containing the bcgeneratestaticcache copyright information file
+ *
+ * @copyright Copyright (C) 1999 - 2014 Brookins Consutling and 2007 Damien POBEL. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ * @package bcgeneratestaticcache
+ */
 
 require 'autoload.php';
 
--- lib/bcstaticcache.php
+++ lib/bcstaticcache.php
@@ -1,12 +1,11 @@
-
 <?php
 /**
- * File containing the BCStaticCache class
+ * File containing the bcstaticcache class file
  *
- * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 1999 - 2013 eZ Systems AS., 1999 - 2014 Brookins Consutling and 2007 Damien POBEL. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
- * @version  2013.5
- * @package kernel
+ * @version 2013.5 (modified)
+ * @package bcgeneratestaticcache
  */
 
 /**
