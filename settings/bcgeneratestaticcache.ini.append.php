7f0b78455b936190fac13bf9dca72d827476365d
Brookins Consulting <info@brookinsconsulting.com>
2023-03-25 03:11:06 +0000

    Updated: Commented out all extension global siteaccess settings

--- settings/bcgeneratestaticcache.ini.append.php
+++ settings/bcgeneratestaticcache.ini.append.php
@@ -1,16 +1,19 @@
 <?php /* #?ini charset="utf-8"?
 
-[BCGenerateStaticCacheSettings]
+# [BCGenerateStaticCacheSettings]
 # Content subtree NodeID to include in static cache content
 # StaticCacheRootNodeID=2
 # Sitemap link default protocol
-Protocol=https
+# Protocol=https
 # Sitemap output directory path. Path to save sitemap into.
 # Default is empty which causes code to use site.ini:[FileSettings] VarDir path
 # Path is relative to the eZ Publish root directory
-Path=
+# Path=
+# Add parent nodeIDs to exclude content tree results from sitemap$
+# ExcludedNodeIDs[]
+# ExcludedNodeIDs[]=341
 
-[SiteAccessSettings]
+# [SiteAccessSettings]
 # Specify every siteaccess a separate sitemap shall be created for.
 # If no siteaccessarray is given, the default siteaccess will be used for generation.
 # If using the generatemultilingual cronjob then this setting is a list of siteaccesses / languages to include in a single sitemap.
@@ -18,24 +21,21 @@ Path=
 # SiteAccessArray[]=de
 # SiteAccessArray[]=en
 
-[Classes]
+# [Classes]
 # Specify include or exclude of objects of classes listed in ClassFilterArray
-ClassFilterType=exclude
+# ClassFilterType=exclude
 
 # Specify object classes to include or exclude from sitemap
-ClassFilterArray[]
+# ClassFilterArray[]
 #ClassFilterArray[]=folder
 #ClassFilterArray[]=article
 #ClassFilterArray[]=image
 #ClassFilterArray[]=forum
 #ClassFilterArray[]=...
 
-[NodeSettings]
+# [NodeSettings]
 # Set this setting to false to include only main node of content objects in sitemap
 # Set this setting to true to include all nodes of content object in sitemap
-Main_Node_Only=false
-# Add parent nodeIDs to exclude content tree results from sitemap$
-ExcludedNodeIDs[]
-#ExcludedNodeIDs[]=341
+# Main_Node_Only=false
 
 */ ?>
